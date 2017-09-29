ca0601fac699bb492f0d02702f86d652dec50976
bloslo <jheru@abv.bg>
2017-09-29 19:16:35 +0200

    Update add.test.js.

--- src/add.test.js
+++ src/add.test.js
@@ -1,6 +1,6 @@
-const add = require('./add');
-describe('add', () => {
-  it('should add two numbers', () => {
-    expect(add(1, 2)).toBe(3);
-  });
+import add from './add';
+
+it ('adds two numbers', () => {
+	expect(add(5, 6)).toEqual(11);
+	expect(add(120, 163)).toEqual(283);
 });
