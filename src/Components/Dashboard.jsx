3bd2a9322014374112c4189b0255ebb803118705
Dean Narendra <dfnarendra@gmail.com>
2017-10-22 17:11:30 +0200

    +Add Logout Login on Dashboard Component

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import {firebaseApp} from '../firebase';
 
 class Dashboard extends Component{
+signOut() {
+  firebaseApp.auth().signOut();
+}
+
   render() {
     return (
-      <div>Hello From Dashboard!</div>
+      <div>
+          <button
+            className="btn btn-danger"
+          >
+            Sign Out
+          </button>
+      </div>
     )
   }
 }
 
-export default Dashboard;
+export default App;
