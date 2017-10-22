5aa6e737c32558bda79e44f50c91c6e6dfd6883e
Dean Narendra <dfnarendra@gmail.com>
2017-10-22 16:56:51 +0200

    +Add Router to the Component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 
+//Importing Componenting Component
+import Dashboard from './Components/Dashboard';
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
 
 ReactDOM.render(
-  <div>Test</div>, document.getElementById('root')
+  <Router path="/" history={browserHistory}>
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+  </Router>, document.getElementById('root')
 )
