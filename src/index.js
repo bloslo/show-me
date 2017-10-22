9e50f5973281a237764a125449c816f2fceaf014
Dean Narendra <dfnarendra@gmail.com>
2017-10-22 17:02:44 +0200

    +Add Welcome Component for the Landing Page. +Set the Firebase Logic for the Signed in and Not Signed in User

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
+import { firebaseApp } from './firebase';
 
 //Importing Componenting Component
 import Dashboard from './Components/Dashboard';
 import SignIn from './Components/SignIn';
 import SignUp from './Components/SignUp';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if(user) {
+    browserHistory.push('/app');
+  } else {
+    browserHistory.replace('/signin');
+  }
+})
 
 ReactDOM.render(
   <Router path="/" history={browserHistory}>
