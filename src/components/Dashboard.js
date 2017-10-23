19bf6bf6810abfc9a3e69bb15cb19946243fe133
Coen Stange <coen.stange@hotmail.com>
2017-10-23 13:13:15 +0200

    + remove not working signout button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,12 +9,7 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <button
-          className="btn btn-danger"
-          onClick={() => this.signOut()}
-        >
-          Sign Out
-        </button>
+        Dashboard
       </div>
     );
   }
