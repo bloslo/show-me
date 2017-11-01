3a6d6e91be84dc29f46748a5ce1e0308576f1084
Dean Narendra <dfnarendra@gmail.com>
2017-11-01 16:32:53 +0100

    +Fix the Eslint +Add Constructor on Player and Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,20 +3,28 @@ import Player from './Player';
 
 
 class Dashboard extends Component {
-  signOut() {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
   }
 
   render() {
     return (
       <div>
         <div>
-        Dashboard
+          Dashboard
         </div>
-        
+        <div className="col-md-4">
+          <Player url="http://40.68.124.79:1776/hls/stream.m3u8" />
+        </div>
+        <div className="col-md-4">
+          Test
+        </div>
+        <div className="col-md-4">
+          Test
+        </div>
       </div>
-
-
     );
   }
 }
