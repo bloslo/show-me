4e894447bc022686b960a5a428814feb43326353
Coen Stange <coen.stange@hotmail.com>
2017-11-03 21:25:28 +0100

    + fix eslint

--- deploy.js
+++ deploy.js
@@ -1,18 +1,19 @@
-var FtpDeploy = require('ftp-deploy');
-var ftpDeploy = new FtpDeploy();
- 
-var config = {
-    username: process.env.USER,
-    password: process.env.PASS, // optional, prompted if none given 
-    host: "hera.fhict.nl",
-    port: 21,
-    localRoot: __dirname + "/build",
-    remoteRoot: "/public_html/",
-    include: [''],
-    exclude: ['.git']
-}
-    
-ftpDeploy.deploy(config, function(err) {
-    if (err) console.log(err)
-    else console.log('finished');
+const FtpDeploy = require('ftp-deploy');// eslint-disable-line import/no-unresolved
+
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+  username: process.env.USER,
+  password: process.env.PASS, // optional, prompted if none given
+  host: 'hera.fhict.nl',
+  port: 21,
+  localRoot: `${__dirname}/build`,
+  remoteRoot: '/public_html/',
+  include: [''],
+  exclude: ['.git'],
+};
+
+ftpDeploy.deploy(config, (err) => {
+  if (err) console.log(err);
+  else console.log('finished');
 });
