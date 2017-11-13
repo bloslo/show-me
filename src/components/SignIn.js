ad7057037a6998b1a28e052b2473ec0a9beb897b
bloslo <jheru@abv.bg>
2017-11-13 15:58:05 +0100

    Remove unused import.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import '../CSS/SignIn.css';
 
 import { sendMessage } from '../actions';
 
